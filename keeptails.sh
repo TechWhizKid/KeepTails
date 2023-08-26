2281ff32d38a7a7636d02e37201e9ac65505381b
TechWhizKid <130601057+TechWhizKid@users.noreply.github.com>
2023-08-27 05:11:17 +0600

    Updated the main script

--- keeptails.sh
+++ keeptails.sh
@@ -1,76 +1,112 @@
 #!/bin/bash
 
-# Function to generate the backup filename with an index
+# Default filenames and paths
+DEFAULT_FILENAME=".gnome_settings"
+BACKUP_PREFIX="gnome_settings_old"
+
+# Function to generate a unique backup filename
 generate_backup_filename() {
-  BACKUP_INDEX=1
-  BACKUP_FILENAME="gnome_settings_old_$BACKUP_INDEX"
-  while [ -f "$BACKUP_FILENAME" ]; do
-    ((BACKUP_INDEX++))
-    BACKUP_FILENAME="gnome_settings_old_$BACKUP_INDEX"
+  local backup_index=1
+  local backup_filename="${BACKUP_PREFIX}_${backup_index}"
+  
+  while [ -f "$backup_filename" ]; do
+    ((backup_index++))
+    backup_filename="${BACKUP_PREFIX}_${backup_index}"
   done
+  
+  echo "$backup_filename"
 }
 
 # Function to display the help message
 display_help() {
-  echo "Usage: $0 [load/backup] [filename]"
-  echo "Options:"
-  echo "  load     : Load gnome settings from backup (default: load from default file)"
-  echo "  backup   : Make a backup of gnome settings (default: use default filename)"
-  echo "  filename : Specify the filename for backup/restore (optional)"
-  echo "  -h, --help : Display this help message"
+  cat << EOF
+Usage: $0 [load/backup] [filename]
+Options:
+  load        : Load gnome settings from backup (default: load from default file)
+  backup      : Make a backup of gnome settings (default: use default filename)
+  filename    : Specify the filename for backup/restore (optional)
+  -h, --help  : Display this help message
+EOF
   exit 0
 }
 
-# Check if -h or --help argument is passed, display help message and exit
+# Function to handle errors
+handle_error() {
+  echo "Error: $1" >&2
+  exit 1
+}
+
+# Check for help argument
 if [[ "$*" == *"-h"* || "$*" == *"--help"* ]]; then
   display_help
 fi
 
-# Ask the user whether to make a backup or load a backup if no arguments are provided
+# Ask for user input if no arguments are provided
 if [[ ! $1 ]]; then
-  printf "\n"
-  echo "Choose an option:"
-  echo "1. Make a backup of gnome settings"
-  echo "2. Load gnome settings from backup"
-  read -p "Enter your choice: " CHOICE
-  printf "\n"
+  PS3="Choose an option and press 'Enter': "
+  options=("Make a backup of gnome settings" "Load gnome settings from backup")
+  select CHOICE in "${options[@]}"; do
+    case $REPLY in
+      1) ACTION="backup"; break ;;
+      2) ACTION="load"; break ;;
+      *) echo "Invalid choice, please select 1 or 2." ;;
+    esac
+  done
+
+  # Prompt for filename only in interactive mode
+  if [[ $ACTION ]]; then
+    read -rp "Enter filename (press Enter to use default): " input_filename
+    FILENAME="${input_filename:-$DEFAULT_FILENAME}"
+
+    if [[ $ACTION == "backup" ]]; then
+      # Make a backup of gnome settings
+      if [ -f "$FILENAME" ]; then
+        backup_filename=$(generate_backup_filename)
+        mv "$FILENAME" "$backup_filename" || handle_error "Failed to rename file"
+        echo "Existing $FILENAME renamed to $backup_filename"
+      fi
+      dconf dump / > "$FILENAME" || handle_error "Failed to create backup"
+      echo "Gnome settings backed up successfully to file: $FILENAME."
+      exit 0
+    else
+      # Load gnome settings from backup
+      if [ -f "$FILENAME" ]; then
+        dconf load -f / < "$FILENAME" || handle_error "Failed to load settings from backup"
+        echo "Settings loaded successfully from file: $FILENAME."
+        exit 0
+      else
+        handle_error "Backup file not found."
+      fi
+    fi
+  fi
 fi
 
-# Determine the action and filename based on command-line arguments or user input
-ACTION="load"
-FILENAME=".gnome_settings"
-
-if [[ $1 == "backup" || $CHOICE == 1 ]]; then
-  ACTION="backup"
-fi
-
-if [[ $2 ]]; then
-  FILENAME="$2"
-fi
+# Set action and filename based on input
+ACTION="${1:-$ACTION}"
+FILENAME="${2:-$DEFAULT_FILENAME}"
 
 if [[ $ACTION == "backup" ]]; then
-  ### Make a backup of gnome settings
+  # Make a backup of gnome settings
   if [ -f "$FILENAME" ]; then
-    generate_backup_filename
-    mv "$FILENAME" "$BACKUP_FILENAME"
-    echo "Existing $FILENAME renamed to $BACKUP_FILENAME"
+    backup_filename=$(generate_backup_filename)
+    mv "$FILENAME" "$backup_filename" || handle_error "Failed to rename file"
+    echo "Existing $FILENAME renamed to $backup_filename"
   fi
-  dconf dump / > "$FILENAME"
-  echo "Gnome settings backed up successfully."
+  dconf dump / > "$FILENAME" || handle_error "Failed to create backup"
+  echo "Gnome settings backed up successfully to file: $FILENAME."
 else
-  ### Load gnome settings from backup
+  # Load gnome settings from backup
   if [ -f "$FILENAME" ]; then
-    dconf load -f / < "$FILENAME"
-    echo "Settings loaded successfully."
+    dconf load -f / < "$FILENAME" || handle_error "Failed to load settings from backup"
+    echo "Settings loaded successfully from file: $FILENAME."
   else
-    echo "Error: Backup file not found."
-    exit 1
+    handle_error "Backup file not found."
   fi
 fi
 
-### Restart the GNOME Shell to apply the changes
+# Restart the GNOME Shell to apply changes
 if dbus-send --type=method_call --dest=org.gnome.Shell /org/gnome/Shell org.gnome.Shell.Eval "string:'global.reexec_self();'"; then
   echo "$0: Finished setting up."
 else
-  echo "Error restarting GNOME Shell."
+  handle_error "Error restarting GNOME Shell."
 fi
